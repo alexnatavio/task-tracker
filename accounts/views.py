cab75260ae9891678b0ce7aeda0f00774e9e433f
Alex <alex.natavio@gmail.com>
2023-06-06 12:36:01 -0400

    Feature 9 complete

--- accounts/views.py
+++ accounts/views.py
@@ -20,7 +20,7 @@ def login_view(request):
             )
             if user is not None:
                 login(request, user)
-                return redirect("home")
+                return redirect("list_projects")
     else:
         form = LoginForm()
     context = {
